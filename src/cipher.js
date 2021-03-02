4eacf5ec8b29ee25cc8cbf8e9ccdc719a864004f
paola mendoza <paolamendoza@paolas-MacBook-Air.local>
2021-03-02 19:44:04 -0300

    subir proyecto por primera vez

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,64 @@
+// // import { decode } from "iconv-lite";
+
+
 const cipher = {
-  // ...
-};
+  encode: (offset, string) => {
+
+
+    let resultado = "";
+    let box = "";
+    if (offset == null || offset == 0 || offset == [] || string == null || string == 0 || string == []) {
+      throw new TypeError('Error, Error'); //para ver si encuentro un error.
+    }
+    for (let i = 0; i < string.length; i++) {
+      //Fórmula letras 
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        box = ((string.charCodeAt(i) - 65 + parseInt(offset)) % 26) + 65;
+      } //mientras que i en charCodeAt  sea >= 65 y i charcodeat sea <=90 le dire a boz 
+
+      //Espacio número 32 del Código ASCII
+      else if (string.charCodeAt(i) === 32) {
+        box = 32;
+      }
+
+
+      resultado += String.fromCharCode(box);
+    }
+
+    return resultado;
+
+  },
+
+  decode: (offset, string) => {
+
+
+    let resultado = "";
+    let box = "";
+    if (offset == null || offset == 0 || offset == [] || string == null || string == 0 || string == []) {
+      throw new TypeError('Error, Error'); //para ver si encuentro un error.
+    }
+    for (let i = 0; i < string.length; i++) {
+      //Fórmula letras 
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        box = ((string.charCodeAt(i) + 65 - parseInt(offset)) % 26) + 65;
+      }
+
+      //Espacio número 32 del Código ASCII
+      else if (string.charCodeAt(i) === 32) {
+        box = 32;
+      }
+
+
+      resultado += String.fromCharCode(box);
+    }
+
+    return resultado;
+
+  },
+}
+
+
+
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+// pasar de una caja a otra
+
+let boton1 = document.getElementById ("btnCifrar"); //guardando el elemento boton
+boton1.addEventListener ("click", pagSig);       //usando el elemento boton
+let boton2 = document.getElementById ("btnDescrifrar");
+boton2.addEventListener ("click", pagSig);
+
+//BACK PAGE
+
+let reset1 = document.getElementById ("reset"); 
+reset1.addEventListener ("click" , reset); //AQUI ESTOY LLAMANDO A LA FUNCION
+function reset (){
+    document.getElementById ("box1").style.display = "block"; //se ve
+    document.getElementById ("pagSig").style.display = "none"; // no se ve
+
+}
+//mostrar pag siguiente
+
+ function pagSig() {
+     document.querySelector('.tituloPie1').style.display = "none";
+     document.querySelector('.nextPage').style.display = "block";
+
+
+
+// // //guardar nombre y ejercicios de como obtener valores usando charCodeAt y string.fromCharCode 
+    
+// //     let valor = document.querySelector(".cajaFunadoaqui").value; 
+// //     let mayuscula = valor.toUpperCase (); 
+// //     let ascii = mayuscula.charCodeAt (0);
+// //     document.querySelector (".cajaResultado").textContent = ascii;    
+// //     let nueva = ascii + 3;
+// //     console.log (nueva);
+// //     let letraNueva = String.fromCharCode(nueva);
+// //     console.log (letraNueva);     
+}    
+
+//boton cifrar con desplazamiento
+
+   document.getElementById("btnCifrar").addEventListener("click", () => {
+   let string = (document.getElementById("caja1").value).toUpperCase(); //guardar en la variable texto lo que se escriba en la caja 1
+//    console.log (string);
+   let offset = parseInt(document.getElementById("offSetCorazon").value); //guardando el offset
+//    console.log (offset);
+
+   let connect = cipher.encode(offset,string);
+   cipher.encode(offset,string);
+   document.getElementById("resultado").innerHTML = connect;
+   });
+
+//boton descifrar con desplazamiento
+
+   document.getElementById("btnDescrifrar").addEventListener("click", () => {
+    let string = (document.getElementById("caja1").value).toUpperCase(); //string es donde voy a poner el mensaje que en id es caja 1
+    let offset = parseInt(document.getElementById("offSetCorazon").value); 
+    // console.log (offset);
+    let connect = cipher.decode(offset,string);
+    cipher.decode(offset,string);
+   document.getElementById("resultado").innerHTML = connect;
+   
+   });
+
+
+//    console.log(cipher);
